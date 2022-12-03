f9f60d0002d05716f48fdbf61e1ae45eae09baba
Aditya Agarwal <50960175+adi611@users.noreply.github.com>
2022-12-03 20:45:38 +0530

    add worldcoin

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,33 @@
 import { WorldIDWidget } from "@worldcoin/id";
+import { useEffect, useState } from "react";
+
 import "./LandingPage.css";
 
 function LandingPage() {
+  const [verified, setVerified] = useState(false);
+  const handleVerification = async (verificationResponse) => {
+    const options = {
+      action_id: "wid_90c20f9fcea5018032f8960046ac8285",
+      signal: "test_signal_01",
+      proof: verificationResponse.proof,
+      nullifier_hash: verificationResponse.nullifier_hash,
+      merkle_root: verificationResponse.merkle_root,
+    };
+
+    const res = await fetch("https://developer.worldcoin.org/api/v1/verify", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(options),
+    });
+
+    const x = await res.json();
+    console.log("Worldcoin response", x);
+
+    console.log("Verified!");
+    setVerified(true);
+  };
   return (
     <div className="landing-bg">
       <main className="landing-main">
@@ -11,10 +37,13 @@ function LandingPage() {
         </section>
         <section className="world-coin-container">
           <WorldIDWidget
-            actionId="wid_staging_4f30f07cabc4d7a66dc6517289c658cf"
+            actionId="wid_90c20f9fcea5018032f8960046ac8285"
             signal="test_signal_01"
             enableTelemetry
-            onSuccess={(verificationResponse) => console.log(verificationResponse)}
+            // onSuccess={(verificationResponse) => console.log(verificationResponse)}
+            onSuccess={(verificationResponse) => {
+              handleVerification(verificationResponse);
+            }}
             onError={(error) => console.log(error)}
             debug={true}
           />
